9f29d83309e7718ec606358697c32b0d55e439f8
zysktomasz <zysk.tomasz95@gmail.com>
2018-05-06 19:17:54 +0200

    setup MVC with OdeToCode.AddFeatureFolder package

---
+++ VotingApp.Web/Features/Home/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VotingApp.Web.Features.Home
+{
+
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "test";
+        }
+    }
+}
