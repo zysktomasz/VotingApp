503674c95f4845f10765cc8e2eaf36880c511b56
zysktomasz <zysk.tomasz95@gmail.com>
2018-05-09 00:17:36 +0200

    add LoginStatusViewComponent

---
+++ VotingApp.Web/Components/LoginStatusViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VotingApp.Core.Models;
+
+namespace VotingApp.Web.Components
+{
+    public class LoginStatusViewComponent : ViewComponent
+    {
+        // code from article: https://andrewlock.net/an-introduction-to-viewcomponents-a-login-status-view-component/
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LoginStatusViewComponent(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (_signInManager.IsSignedIn(HttpContext.User))
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                return View("LoggedIn", user);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
