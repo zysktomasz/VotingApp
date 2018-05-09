5aa8f04a128370138abb6d3df33bad1f46aa510b
zysktomasz <zysk.tomasz95@gmail.com>
2018-05-09 10:25:30 +0200

    add initial PollController, create PollAddViewModel

---
+++ VotingApp.Web/Features/Polls/PollAddViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotingApp.Web.Features.Polls
+{
+    public class PollAddViewModel
+    {
+        [Required]
+        public string Question { get; set; }
+        [Required]
+        public ICollection<string> Answers { get; set; }
+
+    }
+}
---
+++ VotingApp.Web/Features/Polls/PollsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VotingApp.Web.Features.Polls
+{
+    [Authorize]
+    public class PollsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddPoll()
+        {
+            return View();
+        }
+    }
+}
