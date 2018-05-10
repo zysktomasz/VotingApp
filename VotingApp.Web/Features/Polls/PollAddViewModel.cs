b958baf87d38cfc4c0a0da27dea1425fafc83454
zysktomasz <zysk.tomasz95@gmail.com>
2018-05-10 17:33:03 +0200

    add custom validator for AddPoll - not allowing any empty answers (to be refactored)

--- VotingApp.Web/Features/Polls/PollAddViewModel.cs
+++ VotingApp.Web/Features/Polls/PollAddViewModel.cs
@@ -10,8 +10,22 @@ namespace VotingApp.Web.Features.Polls
     {
         [Required]
         public string Question { get; set; }
-        [Required]
+        [EnsureICollectionElementNotNull(ErrorMessage = "Answer cannot be empty.")]
         public ICollection<string> Answers { get; set; }
 
+    }
+
+    public class EnsureICollectionElementNotNullAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var list = value as ICollection<string>;
+
+            if (list.Any(answer => answer == null))
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
